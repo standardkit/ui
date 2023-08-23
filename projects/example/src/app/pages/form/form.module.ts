591008ec28c61fece43cacca782163117f4354f4
Coen Mooij <coenmooij@gmail.com>
2023-08-23 14:51:53 +0200

    add multiple checkboxes

--- projects/example/src/app/pages/form/form.module.ts
+++ projects/example/src/app/pages/form/form.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CheckboxInputPage } from './checkbox-input';
-import { SkCheckboxInputModule, SkButtonModule, SkTopBarModule } from '@standardkit/ui';
+import { SkCheckboxInputModule, SkButtonModule, SkTopBarModule, SkColumnModule, SkRowModule } from '@standardkit/ui';
 
 const PAGES: any[] = [CheckboxInputPage];
 
 @NgModule({
-  imports: [ReactiveFormsModule, SkTopBarModule, SkCheckboxInputModule, SkButtonModule],
+  imports: [ReactiveFormsModule, SkTopBarModule, SkCheckboxInputModule, SkButtonModule, SkColumnModule, SkRowModule],
   declarations: PAGES,
   exports: PAGES,
 })
