4209e69deca734863e6a7a7c4c16ab380fdbf11e
Coen Mooij <coenmooij@gmail.com>
2023-08-23 08:08:34 +0200

    clean up example page and add favicon

--- projects/example/src/app/layout/page/page.layout.ts
+++ projects/example/src/app/layout/page/page.layout.ts
@@ -4,26 +4,25 @@ import { NavigationItemInterface } from '@standardkit/ui';
 @Component({ selector: 'example-page-layout', templateUrl: 'page.layout.html' })
 export class PageLayout {
   public navigationItems: NavigationItemInterface[] = [
-    { icon: 'house', name: 'Homepage', route: 'dashboard' }, // TODO : Figure out pathmatch full option
+    { icon: 'house', name: 'Homepage', route: '', isExact: true },
     {
       icon: 'box-archive',
       name: 'Common',
-      route: 'common', // TODO : Make optional or use as prefix for child routes
-      // TODO : rename subitems to children just like in angular
-      subNavigationItems: [
-        { name: 'accordion', route: 'common/accordion' },
-        { name: 'alert', route: 'common/alert' },
+      route: 'common', // TODO : Make as prefix for child routes
+      children: [
+        { name: 'Accordion', route: 'common/accordion' },
+        { name: 'Alert', route: 'common/alert' },
       ],
     },
     {
       icon: 'code-fork',
       name: 'Core',
       route: 'core',
-      subNavigationItems: [
-        { name: 'bar', route: 'core/bar' },
-        { name: 'button', route: 'core/button' },
-        { name: 'icon', route: 'core/icon' },
-        { name: 'link', route: 'core/link' },
+      children: [
+        { name: 'Bar', route: 'core/bar' },
+        { name: 'Button', route: 'core/button' },
+        { name: 'Icon', route: 'core/icon' },
+        { name: 'Link', route: 'core/link' },
       ],
     },
   ];
