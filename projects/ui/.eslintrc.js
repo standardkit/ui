733ff1ed0d5fd91cf9066befdc5616f61abe2032
Coen Mooij <coenmooij@gmail.com>
2024-12-29 14:18:32 +0100

    docs: add todos for updating prefix

--- projects/ui/.eslintrc.js
+++ projects/ui/.eslintrc.js
@@ -12,8 +12,10 @@ module.exports = {
       rules: {
         "@angular-eslint/directive-selector": [
           "error",
+          // TODO : Remove sk when components updated to new style
           { type: "attribute", prefix: ["sk", "ui"], style: "camelCase" },
         ],
+        // TODO : Remove sk when components updated to new style
         "@angular-eslint/component-selector": ["error", { type: "element", prefix: ["sk", "ui"], style: "kebab-case" }],
         "@angular-eslint/component-class-suffix": ["error", { suffixes: ["Component"] }],
         "@angular-eslint/component-max-inline-declarations": ["error", { template: 1 }],
