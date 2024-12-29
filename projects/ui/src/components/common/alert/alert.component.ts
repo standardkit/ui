4bbcb5b20c5145d9982ac9497764769cebeb4fd0
Coen Mooij <coenmooij@gmail.com>
2024-12-29 13:48:33 +0100

    feat: refactor alert to new style

--- projects/ui/src/components/common/alert/alert.component.ts
+++ projects/ui/src/components/common/alert/alert.component.ts
@@ -1,14 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Style } from '../../../constants';
 import { StyleType } from '../../../types';
 
 @Component({
-  selector: 'sk-alert',
+  selector: 'ui-alert',
   templateUrl: 'alert.component.html',
-  styleUrls: ['alert.component.scss'],
-  standalone: false,
+  styleUrl: 'alert.component.scss',
+  imports: [NgIf],
 })
-export class SkAlertComponent {
+export class UiAlert {
   @Input() public type: StyleType = Style.Error;
   @Input() public hasNoShadow: boolean = false;
 }
