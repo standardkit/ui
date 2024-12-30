418ff60eeb845c8ff9763513c2ffc0b2144fa482
Coen Mooij <coenmooij@gmail.com>
2024-12-30 11:58:11 +0100

    fix: add missing jumbotron import

--- projects/example/src/app/pages/home/home.module.ts
+++ projects/example/src/app/pages/home/home.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
-import { SkRowModule, SkTopBarModule, UiBottomBar, UiBreadcrumbs, UiButton, UiColumn } from '@standardkit/ui';
+import {
+  SkRowModule,
+  SkTopBarModule,
+  UiBottomBar,
+  UiBreadcrumbs,
+  UiButton,
+  UiColumn,
+  UiJumbotron,
+} from '@standardkit/ui';
 import { HomePage } from './home.page';
 
 @NgModule({
-  imports: [UiBottomBar, UiButton, SkRowModule, SkTopBarModule, UiBreadcrumbs, UiColumn],
+  imports: [UiBottomBar, UiButton, SkRowModule, SkTopBarModule, UiBreadcrumbs, UiColumn, UiJumbotron],
   declarations: [HomePage],
   exports: [HomePage],
 })
